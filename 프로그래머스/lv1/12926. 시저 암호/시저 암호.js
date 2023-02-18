097b99293ef4defedc044c8199e9706fb504f6d1
최수빈 <107169675+123456soobin-choi@users.noreply.github.com>
2023-02-19 00:41:07 +0900

    [level 1] Title: 시저 암호, Time: 30.37 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,20 @@
+function solution(s, n) {
+    let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let lower = "abcdefghijklmnopqrstuvwxyz";
+    let answer= '';
+
+    for(var i =0; i <s.length; i++){
+        var text = s[i];
+        if(text == ' ') {
+            answer += ' '; 
+            continue;
+        }
+        let textArr = upper.includes(text) ? upper : lower;
+        let index = textArr.indexOf(text)+n;
+        if(index >= textArr.length){
+            index -= textArr.length; 
+        }
+        answer += textArr[index];
+    }
+    return answer;
+}

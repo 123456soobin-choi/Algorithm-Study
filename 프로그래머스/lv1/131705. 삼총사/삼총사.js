2647b988fa85d344b25a78a700b0f117f1141932
최수빈 <107169675+123456soobin-choi@users.noreply.github.com>
2023-06-01 17:50:58 +0900

    [level 1] Title: 삼총사, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -0,0 +1,14 @@
+function solution(numbers) {
+    let answer = 0;
+    for(let i=0; i<numbers.length; i++){
+        for(let j=i+1; j<numbers.length; j++){
+            for(let k=j+1; k<numbers.length; k++){
+                if(numbers[i] + numbers[j] + numbers[k] === 0){
+                    answer += 1;
+                } 
+            }
+        }
+
+    }
+    return answer;
+}

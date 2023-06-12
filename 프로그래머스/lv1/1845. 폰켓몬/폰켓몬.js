07fffe460186a82ddcf804830952253013ca7750
최수빈 <107169675+123456soobin-choi@users.noreply.github.com>
2023-06-12 23:56:15 +0900

    [level 1] Title: 폰켓몬, Time: 0.79 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -0,0 +1,17 @@
+function solution(nums) {
+    let answer = 0;
+    let count = nums.length / 2;
+    
+    // 중복 제거
+    let arr = nums.filter((element, index) => {
+        return nums.indexOf(element) === index;
+    });
+    
+    if(arr.length >= count){
+        answer = count;
+    }else{
+        answer = arr.length;
+    }
+    
+    return answer;
+}

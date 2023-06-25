a94e2bded503e586b3b47b149f39577330d53fd9
최수빈 <107169675+123456soobin-choi@users.noreply.github.com>
2023-06-26 00:30:42 +0900

    [unrated] Title: 과일 장수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,16 @@
+function solution(k, m, score) {
+    let answer = 0;
+
+    if(score.length < m){
+        return 0;
+    }
+    
+    score.sort((a,b) => a-b);
+    
+    while(score.length >= m){
+        let box = score.splice(score.length - m, m);
+        answer += Math.min(...box)*m;
+    }
+    
+    return answer;
+}

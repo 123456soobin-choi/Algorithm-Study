ca06a51a132e824314c5601ae3d6b02e6393ce68
최수빈 <107169675+123456soobin-choi@users.noreply.github.com>
2023-07-04 00:15:16 +0900

    [unrated] Title: 덧칠하기, Time: 3.46 ms, Memory: 40 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -0,0 +1,13 @@
+function solution(n, m, section) {
+    let answer = 0;
+    let painted = 0;
+    
+    for (let element of section) {
+        if(painted < element){
+            answer++;
+            painted = element + m -1;
+        }
+    }
+    
+    return answer;
+}

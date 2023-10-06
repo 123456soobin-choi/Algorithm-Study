cdb279df15889e2ff8617793f51d89ab24eae753
최수빈 <107169675+123456soobin-choi@users.noreply.github.com>
2023-10-06 22:34:50 +0900

    [level 2] Title: 땅따먹기, Time: 25.58 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
@@ -0,0 +1,31 @@
+function solution(land) {
+  let answer = [];
+  let len = land.length;
+  for (let i = 1; i < len; i++) {
+    land[i][0] += Math.max(
+      land[i - 1][1],
+      land[i - 1][2],
+      land[i - 1][3]
+    );
+    land[i][1] += Math.max(
+      land[i - 1][0],
+      land[i - 1][2],
+      land[i - 1][3]
+    );
+    land[i][2] += Math.max(
+      land[i - 1][0],
+      land[i - 1][1],
+      land[i - 1][3]
+    );
+    land[i][3] += Math.max(
+      land[i - 1][0],
+      land[i - 1][1],
+      land[i - 1][2]
+    );
+  }
+  answer = land[land.length - 1];
+
+  return Math.max(...answer);
+}
+
+// land[i] max값 index 와 같지 않은 land[i+1] 의 max값

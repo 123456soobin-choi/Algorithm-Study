6bdbb3825215b016016f902d3cffdd290a96d4fe
최수빈 <107169675+123456soobin-choi@users.noreply.github.com>
2023-12-26 00:21:04 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 11.15 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -0,0 +1,43 @@
+function solution(files) {
+    var arr = [];
+    
+   for(let i = 0; i < files.length; i++){
+       for(let j = 0; j < files[i].length; j++){
+           if(j===0){
+               arr.push({"HEAD":files[i][0], "NUMBER":"", "TAIL": ""});
+           }else if((files[i][j].charCodeAt() < 48 || files[i][j].charCodeAt() > 57) && arr[i]["NUMBER"] === "" ){
+               arr[i]["HEAD"] += files[i][j]; 
+           }else if(files[i][j].charCodeAt() >= 48 && files[i][j].charCodeAt() <= 57 && arr[i]["TAIL"]===""){
+               arr[i]["NUMBER"] += files[i][j];
+           }else{
+               arr[i]["TAIL"] += files[i][j];
+           }
+       }
+   }
+  
+    arr.sort(function (a, b) {
+    let x = a.HEAD.toLowerCase();
+    let y = b.HEAD.toLowerCase();
+    let x2 = Number(a.NUMBER);
+    let y2 = Number(b.NUMBER);
+    if (x < y) {
+        return -1;
+    }
+    if (x > y) {
+        return 1;
+    } else{
+        if( x2 < y2 ){
+            return -1;
+        } if( x2 > y2 ){
+            return 1;
+        }
+    }
+    return 0;
+});
+    
+   return arr.map(v=>v.HEAD+v.NUMBER+v.TAIL);
+};
+
+// 대소문자 구분 없이 HEAD 기준 사전 순서 정렬
+// NUMBER 제일 앞의 0 무시하고 숫자 오름차순 정렬
+// TAIL 그대로 붙임
